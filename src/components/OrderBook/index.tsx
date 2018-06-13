cac7b45587243ed491527cf654be60377081a41b
Elliot Plant <elliotaplant@gmail.com>
2018-06-13 18:22:43 -0500

    Get bids showing up

--- src/components/OrderBook/index.tsx
+++ src/components/OrderBook/index.tsx
@@ -7,9 +7,14 @@ import './index.css';
 // Maybe get the ticker for each order book?
 const OrderBook = ({asks, bids}: any) => (<div className="order-book">
   <div className="order-book-header">BTC/USD</div>
+  Bids:
+  {console.log('bids', bids) || bids && ordersAsList(bids).map(({price, size}: any, index: number) => (
+    <div key={index}>Price: {price}, Size: {size}</div>
+  ))}
+  Asks:
   {asks && ordersAsList(asks).map(({price, size}: any, index: number) => (
     <div key={index}>Price: {price}, Size: {size}</div>
   ))}
 </div>);
 
-export default connect(({asks, header}: any) => ({asks, header}))(OrderBook);
+export default connect(({asks, bids}: any) => ({asks, bids}))(OrderBook);
