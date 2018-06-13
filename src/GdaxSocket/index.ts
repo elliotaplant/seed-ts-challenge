deb50ccfbaee499600f70b993a0cd852e2d21056
Elliot Plant <elliotaplant@gmail.com>
2018-06-13 13:54:09 -0500

    Add gdax

---
+++ src/GdaxSocket/index.ts
@@ -0,0 +1,25 @@
+import Gdax from 'gdax';
+
+interface ISocketData {
+  moo: string;
+}
+
+interface ISocketError {
+  moo: string;
+}
+
+class GdaxSocket {
+  constructor(
+    private onMessage: (data: ISocketData) => void,
+    private onError: (err: ISocketError) => void,
+    private onClose: () => void
+  ) { }
+
+  public init() {
+    const websocket = new Gdax.WebsocketClient(['BTC-USD', 'ETH-USD']);
+
+    websocket.on('message', this.onMessage);
+    websocket.on('error', this.onError);
+    websocket.on('close', this.onClose);
+  }
+}
