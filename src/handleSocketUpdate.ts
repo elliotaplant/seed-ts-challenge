d435f5b36017a0afa6c5037cf728f7684f90ed68
Elliot Plant <elliotaplant@gmail.com>
2018-06-14 12:06:15 -0500

    Remove log

--- src/handleSocketUpdate.ts
+++ src/handleSocketUpdate.ts
@@ -11,7 +11,5 @@ export default function handleSocketUpdate(update: any) {
     return console.error('Unable to parse message as JSON');
   }
 
-  console.log('asJson', asJson);
-
   store.dispatch(updateOrdersAction(asJson));
 }
