99d70f7d7957f9edda8fe94bc85b1e0fc0fbcc06
Elliot Plant <elliotaplant@gmail.com>
2018-06-14 12:11:54 -0500

    Showing bids and asks

--- src/components/OrderBook/index.tsx
+++ src/components/OrderBook/index.tsx
@@ -1,18 +1,28 @@
 import * as React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { IOrder, IOrdersUpdate } from '../../types';
 
 import './index.css';
 
-// Maybe get the ticker for each order book?
-const OrderBook = ({asks, bids}: any) => (<div className="order-book">
+interface IOrdersProps {
+  orders: IOrder[]
+}
+
+const Orders = ({ orders }: IOrdersProps) => {
+  return orders ? (
+    <div>
+      {orders.map(({ price, size }) => <div key={price}>Price: {price}, Size: {size}</div>)}
+    </div>
+  ) : null;
+}
+
+const Asks = connect(({ asks: orders }: IOrdersUpdate) => ({ orders }))(Orders);
+const Bids = connect(({ bids: orders }: IOrdersUpdate) => ({ orders }))(Orders);
+
+const OrderBook = () => (<div className="order-book">
   <div className="order-book-header">BTC/USD</div>
-  Bids:
-  {bids && bids.map(({price, size}: any) => (
-    <div key={price}>Price: {price}, Size: {size}</div>
-  ))}
-  Asks:
-  {asks && asks.map(({price, size}: any) => (
-    <div key={price}>Price: {price}, Size: {size}</div>
-  ))}
+  Bids: <Bids />
+  Asks: <Asks />
 </div>);
-export default connect(({asks, bids}: any) => ({asks, bids}))(OrderBook);
+
+export default OrderBook;
