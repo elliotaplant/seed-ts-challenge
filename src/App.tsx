307f5352aeeb7d25498166fb8685e3037250856a
Elliot Plant <elliotaplant@gmail.com>
2018-06-16 11:39:11 -0500

    Make order book only thing on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Websocket from 'react-websocket';
-import Header from './components/Header';
+import './App.css';
 import OrderBook from './components/OrderBook';
 
 import handleSocketUpdate from './handleSocketUpdate';
@@ -9,7 +9,6 @@ import handleSocketUpdate from './handleSocketUpdate';
 // Main App class to gather subcomponents
 const App = () => (<div className="app">
   <Websocket url="ws://127.0.0.1:1337" onMessage={handleSocketUpdate} />
-  <Header />
   <OrderBook />
 </div>
 );
--- src/components/Header/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import './index.css';
-
-const Header = () => <h1 className="header">Seed CX Order Book</h1>;
-
-export default Header;
