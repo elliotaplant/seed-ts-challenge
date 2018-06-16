f87bf6ef6baad751f0e09af8a5a8488a1f9aeb04
Elliot Plant <elliotaplant@gmail.com>
2018-06-16 14:25:29 -0500

    Calc wsurl

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.css';
 import OrderBook from './components/OrderBook';
 import handleSocketUpdate from './handleSocketUpdate';
 
+// Get the WebSocket url based on the environment
+const wsUrl = process.env.NODE_ENV === 'development' ? 'ws://127.0.0.1:3030' : `ws://0.0.0.0:${process.env.PORT}`;
+
 // Main App class to gather subcomponents
 const App = () => (<div className="app">
-  <Websocket url="ws://127.0.0.1:3030" onMessage={handleSocketUpdate} />
+  <Websocket url={wsUrl} onMessage={handleSocketUpdate} />
   <OrderBook />
 </div>
 );
